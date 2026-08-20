678899f20b810189602748dcbf2e12745213b545
sooieong <sooieong@naver.com>
2026-08-20 11:39:04 +0900

    Feat: 스플래시 시작 창 추가

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,14 +1,27 @@
-﻿using System.Configuration;
-using System.Data;
+﻿using System;
+using System.Threading.Tasks;
 using System.Windows;
 
 namespace Ship_Progress
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
-    }
+        private async void Application_Startup(object sender, StartupEventArgs e)
+        {
+            // 1. 스플래시 시작 창 생성 및 표시
+            SplashWindow splash = new SplashWindow();
+            splash.Show();
 
+            // 2. 초기 로딩 대기시간 (2초)
+            await Task.Delay(2000);
+
+            // 3. 메인 화면 생성 및 출력
+            MainWindow mainView = new MainWindow();
+            this.MainWindow = mainView;
+            mainView.Show();
+
+            // 4. 스플래시 창 닫기
+            splash.Close();
+        }
+    }
 }
---
+++ SplashWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ship_Progress
+{
+    public partial class SplashWindow : Window
+    {
+        public SplashWindow()
+        {
+            InitializeComponent(); // partial 키워드가 빠져있으면 안 됩니다!
+        }
+    }
+}
